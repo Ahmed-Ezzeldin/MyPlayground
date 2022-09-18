7a37e5d7a818b2e4974b202ed442ae710877c54d
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-09-18 13:31:52 +0200

    array

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,13 @@
 import Cocoa
 
 var greeting = "Hello, playground"
+
+var beatles: [Any] = ["John", "Paul", "George", "Ringo"]
+print(beatles[2])
+beatles.append("Ahmed")
+beatles.append(223)
+beatles.append(true)
+beatles.append(false)
+beatles.append(0.5)
+print(beatles)
+
