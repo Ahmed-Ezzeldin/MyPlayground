38735c61ba6851c1dc6b285b9ed1f79ff84e088f
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-09-25 15:59:26 +0200

    function part 2

--- PlaygroundTest.playground/Resources/test.swift
+++ PlaygroundTest.playground/Resources/test.swift
@@ -209,6 +209,73 @@ func printTimesTables(for number: Int) {
 }
 printTimesTables(for: 5)
 
+print("\n=====================================================> Default values for parameters \n")
+
+func printTimesTables(for number: Int, end: Int = 3) {
+    for i in 1...end {
+        print("\(i) x \(number) = \(i * number)")
+    }
+    print("----------------")
+}
+
+printTimesTables(for: 2, end: 5)
+printTimesTables(for: 2)
+
+print("\n=====================================================> Handle errors in functions \n")
+enum PasswordError: Error {
+    case short, obvious
+}
+
+func checkPassword(_ password: String) throws -> String {
+    if password.count < 5 {
+        throw PasswordError.short
+    }
+
+    if password == "12345" {
+        throw PasswordError.obvious
+    }
+
+    if password.count < 8 {
+        return "OK"
+    } else if password.count < 10 {
+        return "Good"
+    } else {
+        return "Excellent"
+    }
+}
+
+let string = "12345"
+
+do {
+    let result = try checkPassword(string)
+    print("Password rating: \(result)")
+} catch PasswordError.short {
+    print("Please use a longer password.")
+} catch PasswordError.obvious {
+    print("I have the same combination on my luggage!")
+} catch {
+    print("There was an error.")
+}
+
+print("\n=====================================================> Handle errors in functions \n")
+func doImportantWork(first: () -> Void, second: () -> Void, third: () -> Void) {
+    print("About to start first work")
+    first()
+    print("About to start second work")
+    second()
+    print("About to start third work")
+    third()
+    print("Done!")
+}
+
+doImportantWork {
+    print("This is the first work")
+} second: {
+    print("This is the second work")
+} third: {
+    print("This is the third work")
+}
+
 // MARK: - ===================================================================> Temp
 // MARK: - ===================================================================> Temp
 // MARK: - ===================================================================> Temp
