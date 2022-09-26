b09242ac8d5d214c40ee988906d3223482f95c81
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-09-26 15:27:00 +0200

    access control

--- PlaygroundTest.playground/Resources/test.swift
+++ PlaygroundTest.playground/Resources/test.swift
@@ -344,6 +344,48 @@ struct Player {
 
 let player = Player(name: "Megan R", number: 15)
 
+// MARK: - ===================================================================> Access control
+print("\n=====================================================> Access control \n")
+struct BankAccount {
+    private var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+print("\n=====================================================> Static properties and methods \n")
+struct School {
+    static var studentCount = 0
+
+    static func add(student: String) {
+        print("\(student) joined the school.")
+        studentCount += 1
+    }
+}
+
+School.add(student: "Taylor Swift")
+print(School.studentCount)
+
+
 // MARK: - ===================================================================> Temp
 // MARK: - ===================================================================> Temp
 // MARK: - ===================================================================> Temp
