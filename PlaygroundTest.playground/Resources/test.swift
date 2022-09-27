7841143999405a90a6527a2eab623e2e25b0274f
Ahmed-Ezzeldin <ahmed.ezzeldin.ibrahim@gmail.com>
2022-09-27 13:23:15 +0200

    Classes

--- PlaygroundTest.playground/Resources/test.swift
+++ PlaygroundTest.playground/Resources/test.swift
@@ -385,6 +385,120 @@ struct School {
 School.add(student: "Taylor Swift")
 print(School.studentCount)
 
+// MARK: - ===================================================================> Classes
+print("\n=====================================================> create your own classes \n")
+class Game {
+    var score = 0 {
+        didSet {
+            print("Score is now \(score)")
+        }
+    }
+}
+
+var newGame = Game()
+newGame.score += 10
+
+print("\n=====================================================> Class inherit from another \n")
+class Employee {
+    let hours: Int
+    
+    init(hours: Int) {
+        self.hours = hours
+    }
+    func printSummary() {
+        print("I work \(hours) hours a day.")
+    }
+}
+class Developer: Employee {
+    func work() {
+        print("I'm writing code for \(hours) hours.")
+    }
+    override func printSummary() {
+        print("""
+              "I'm a developer who will sometimes work \(hours) hours a day,
+              but other times spend hours arguing about whether code should be indented using tabs or spaces.
+            """)
+    }
+}
+
+//mark class as "final" This means the class itself can inherit from other things, but can’t be used to inherit from
+final class Manager: Employee {
+    func work() {
+        print("I'm going to meetings for \(hours) hours.")
+    }
+}
+
+let robert = Developer(hours: 8)
+let joseph = Manager(hours: 10)
+robert.work()
+joseph.work()
+
+let novall = Developer(hours: 8)
+novall.printSummary()
+
+print("\n=====================================================> Add initializers for classes \n")
+class Vehicle {
+    let isElectric: Bool
+
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+    }
+}
+class Car: Vehicle {
+    let isConvertible: Bool
+
+    init(isElectric: Bool, isConvertible: Bool) {
+        self.isConvertible = isConvertible
+        super.init(isElectric: isElectric)
+    }
+}
+let teslaX = Car(isElectric: true, isConvertible: false)
+
+print("\n=====================================================> Copy classes \n")
+class User {
+    var username = "Anonymous"
+    
+    func copy() -> User {
+        let user = User()
+        user.username = username
+        return user
+    }
+}
+var user1 = User()
+var user2 = user1
+var user3 = user1.copy()
+user2.username = "Taylor"
+
+print(user1.username) // copy by reference
+print(user2.username) // copy by reference
+print(user3.username) // copy by value
+
+print("\n=====================================================> Create a deinitializer for a class \n")
+class Player {
+    let id: Int
+
+    init(id: Int) {
+        self.id = id
+        print("Player \(id): I'm alive!")
+    }
+
+    deinit {
+        print("Player \(id): I'm dead!")
+    }
+}
+
+var players = [Player]()
+
+for i in 1...3 {
+    let player = Player(id: i)
+    print("Player \(player.id): I'm in control!")
+    players.append(player)
+}
+
+print("Loop is finished!")
+players.removeAll()
+print("Array is clear!")
+
 
 // MARK: - ===================================================================> Temp
 // MARK: - ===================================================================> Temp
